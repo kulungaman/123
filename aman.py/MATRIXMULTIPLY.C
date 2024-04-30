0ecf93562fedb1bdb2559686aadaca5054d1c41f
kulungaman <168289714+kulungaman@users.noreply.github.com>
2024-04-30 05:10:31 -0700

    bk

---
+++ aman.py/MATRIXMULTIPLY.C
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+void multiplyMatrices(int mat1[MAX_ROWS][MAX_COLS], int mat2[MAX_ROWS][MAX_COLS], int result[MAX_ROWS][MAX_COLS], int rows1, int cols1, int cols2) {
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int mat1[MAX_ROWS][MAX_COLS], mat2[MAX_ROWS][MAX_COLS], result[MAX_ROWS][MAX_COLS];
+    int rows1, cols1, rows2, cols2;
+
+    printf("Enter the number of rows and columns of first matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    printf("Enter the number of rows and columns of second matrix: ");
+    scanf("%d %d", &rows2, &cols2);
+
+    if (cols1 != rows2) {
+        printf("Matrix multiplication not possible.\n");
+        return 0;
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    multiplyMatrices(mat1, mat2, result, rows1, cols1, cols2);
+
+    printf("Resultant matrix after multiplication:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ aman.py/aman1.py
@@ -0,0 +1,1 @@
+printf aman kulung in the name of the good in front of the name of the good in the name of the good in front fof the social bondings in the name of the good natural in the name of good 
